c857852deda92ee3bc342aa57eacb97f8786243a
Si-Hyeak-KANG <zlcls456@gmail.com>
2022-07-11 23:57:20 +0900

    no message

--- src/main/java/jpa/practice/response/ErrorResponse.java
+++ src/main/java/jpa/practice/response/ErrorResponse.java
@@ -1,3 +1,55 @@
 package jpa.practice.response;
 
-public class ErrorReseponse
+import lombok.Getter;
+
+import javax.validation.ConstraintViolation;
+import java.util.List;
+
+public class ErrorResponse {
+
+    private int status;
+    private String message;
+    private List<FieldError> fieldErrors;
+    private List<ConstraintViolationError> violationErrors;
+
+    private ErrorResponse(int Status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    private ErrorResponse(final List<FieldError> fieldErrors,
+                          final List<ConstraintViolationError> violationErrors) {
+        this.fieldErrors = fieldErrors;
+        this.violationErrors = violationErrors;
+    }
+
+    @Getter
+    public static class FieldError {
+        private String field;
+        private Object rejectedValue;
+        private String reason;
+
+        public FieldError(String field, Object rejectedValue, String reason) {
+            this.field = field;
+            this.rejectedValue = rejectedValue;
+            this.reason = reason;
+        }
+
+
+    }
+
+    @Getter
+    public static class ConstraintViolationError {
+
+        private String propertyPath;
+        private Object rejectedValue;
+        private String reason;
+
+        public ConstraintViolationError(String propertyPath, Object rejectedValue, String reason) {
+            this.propertyPath = propertyPath;
+            this.rejectedValue = rejectedValue;
+            this.reason = reason;
+        }
+    }
+
+}
